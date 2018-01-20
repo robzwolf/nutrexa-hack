4a7874bd9e4e2df5b9693fb2f0e6d177244185b5
amsihralhan <amish.jimmy@gmail.com>
2018-01-20 17:01:52 +0000

    Fixed conflict

--- database.js
+++ database.js
@@ -1,16 +1,31 @@
-// database.js
+var AWS = require("aws-sdk");
+var fs = require('fs');
 
+AWS.config.update({
+    region: "us-west-2",
+    endpoint: "http://localhost:8000"
+});
 
-module.exports = {
-    addToDB: function(item) {
-        // Do some stuff to add `item` to the dynamoDB
-    },
-    helloWorld: function() {
-        console.log("Hello, world");
-    }
-}
+var docClient = new AWS.DynamoDB.DocumentClient();
 
+console.log("Importing movies into DynamoDB. Please wait.");
 
-function connectToDB() {
-    // Do some stuff
-}
+var allMovies = JSON.parse(fs.readFileSync('moviedata.json', 'utf8'));
+allMovies.forEach(function(movie) {
+    var params = {
+        TableName: "Movies",
+        Item: {
+            "year":  movie.year,
+            "title": movie.title,
+            "info":  movie.info
+        }
+    };
+
+    docClient.put(params, function(err, data) {
+       if (err) {
+           console.error("Unable to add movie", movie.title, ". Error JSON:", JSON.stringify(err, null, 2));
+       } else {
+           console.log("PutItem succeeded:", movie.title);
+       }
+    });
+});
