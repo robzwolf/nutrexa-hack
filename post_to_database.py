592be06f35df868f8edd7bec2e3899d8576ae8c5
amsihralhan <amish.jimmy@gmail.com>
2018-01-20 23:41:16 +0000

    added check_item_existance

--- post_to_database.py
+++ post_to_database.py
@@ -2,10 +2,28 @@
 
 def check_item_existence(food_item):
     # Check the item exists
-    print("check_item_existence called", food_item)
+
+    dynamodb = boto3.resource("dynamodb")
+
+    table = dynamodb.Table('Food_Items')
+    
+    response = table.query(
+    KeyConditionExpression=Key('Identifier').eq(food_item[0])
+    )
+
+    print("check_item_existence called", food_item[0])
+    
+    print(response)
+
+    if(len(response) == 0):
+        return False
+
     return True
     
+
+
 def add_item_to_DB(food_item):
     # Add to Food_Items DB
     print("add_item_to_DB called", food_item)
-    
+
+    print(json.dumps(response["Item"], indent=4, cls=DecimalEncoder))
