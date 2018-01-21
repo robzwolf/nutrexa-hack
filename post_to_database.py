a97c10e290a78ac9b99663a7638ddfce26fe6b2e
amsihralhan <amish.jimmy@gmail.com>
2018-01-21 10:32:14 +0000

    adding calories

--- post_to_database.py
+++ post_to_database.py
@@ -67,7 +67,7 @@ def updateOldFoodInFoodConsumptionTable(food_info):
             'User': userName,
             'Date': date
         },
-        UpdateExpression="set " +food_info[2]+ " = " +food_info[2]+ " + :quantity,  Potasium = Potasium + :valOfPotasium, Sodium = Sodium + :valOfSodium",
+        UpdateExpression="set " +food_info[2]+ " = " +food_info[2]+ " + :quantity,  Potasium = Potasium + :valOfPotasium, Sodium = Sodium + :valOfSodium, Calories = :valOfSodium + :valOfPotasium",
         ExpressionAttributeValues={
             ':quantity' : food_info[3],
             ':valOfPotasium' : food_info[4],
