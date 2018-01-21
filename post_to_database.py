b95c1a02695606179d66c5e13412b33d07d59a53
amsihralhan <amish.jimmy@gmail.com>
2018-01-21 02:48:53 +0000

    adding food item again

--- post_to_database.py
+++ post_to_database.py
@@ -67,9 +67,8 @@ def updateFoodConsumptionTable(food_info):
             'User': userName,
             'Date': date
         },
-        UpdateExpression="set :foodItem = :quantity,  Potasium = Potasium + :valOfPotasium, Sodium = Sodium + :valOfSodium",
+        UpdateExpression="set " +food_info[2]+ " = :quantity,  Potasium = Potasium + :valOfPotasium, Sodium = Sodium + :valOfSodium",
         ExpressionAttributeValues={
-            ':foodItem' : food_info[2],
             ':quantity' : food_info[3],
             ':valOfPotasium' : food_info[4],
             ':valOfSodium' : food_info[5]
